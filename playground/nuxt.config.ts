739640117ccb8f59fa79f5bd4d4c00aa29b2deb4
Galabin <gvasilev094@gmail.com>
2025-06-15 15:53:25 +0300

    Update

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig(
   {
-    modules: ['../src/module'],
+    modules: ['../src/module', 'nuxt-required-env'],
     devtools: { enabled: true },
     runtimeConfig: {
       STRIPE_KEY: process.env.STRIPE_KEY,
